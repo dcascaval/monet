ec48994cad1da0db56a2e2d22bea4133fa6f6684
Dan Cascaval <dan@cascaval.us>
2021-06-01 16:47:22 -0400

    rm performance tracing to fix classcast on chrome

--- src/main/scala/monet/optimize.scala
+++ src/main/scala/monet/optimize.scala
@@ -21,23 +21,21 @@ def optimize(
 ): js.Array[Double] =
   val opt = new nlopt.Optimize(nlopt.Algorithm.LD_SLSQP, initial.length)
 
-  var f_evals = 0
-  var g_evals = 0
+  // var f_evals = 0
+  // var g_evals = 0
 
   opt.setMinObjective((x, grad) =>
     val result = f(x)
-    f_evals += 1
+    // f_evals += 1
     if (grad != null)
       val _grad = g(x)
       for ((g,i) <- _grad.zipWithIndex)
         grad(i) = g
-      g_evals += 1
+      // g_evals += 1
     result
   , 1e-3)
 
-  val t0 = performance.now()
   val res = opt.optimize(initial.toJSArray)
-  val t1 = performance.now()
   // println(s"Optimized in $f_evals iterations ($g_evals gradient iters) [${t1-t0}ms]")
   nlopt.GC.flush()
   res.x
